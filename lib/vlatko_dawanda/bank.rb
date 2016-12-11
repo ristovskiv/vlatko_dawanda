633c414c4bbefac2f6047057c5b39f8c3a7a5beb
Vlatko Ristovski <ristovski.vlatko234@gmail.com>
2016-12-11 19:29:49 +0100

    feat: bank find currencies

--- lib/vlatko_dawanda/bank.rb
+++ lib/vlatko_dawanda/bank.rb
@@ -9,13 +9,31 @@ module VlatkoDawanda
       @currencies ||= {}
     end
 
+    def find_currency(search)
+      currency = case search
+      when ::String, ::Symbol
+        currencies[currency_id(search)]
+      when Currency
+        found = currencies[currency_id(search.iso_code)]
+        raise UnknownCurrency.new('currency not matching the rate') unless found == search
+        found
+      end
+
+      raise UnknownCurrency.new('currency not found') if currency.nil?
+      currency
+    end
+
     private
 
     def parse_currencies(base_currency, currencies)
       currencies.merge({base_currency => 1}).inject({}) do |memo, (key,value)|
-        memo[key.downcase.to_sym] = Currency.new({iso_code: key, rate: value})
+        memo[currency_id(key)] = Currency.new({iso_code: key, rate: value})
         memo
       end
+    end
+
+    def currency_id(value)
+      value.downcase.to_sym
     end
 
   end
--- spec/vlatko_dawanda/bank_spec.rb
+++ spec/vlatko_dawanda/bank_spec.rb
@@ -7,7 +7,7 @@ describe VlatkoDawanda::Bank do
   end
 
   context 'conversion rates' do
-    before do
+    before(:example) do
       subject.conversion_rates('EUR', {'USD' => 1.11, 'Bitcoin' => 0.0047})
     end
 
@@ -21,6 +21,41 @@ describe VlatkoDawanda::Bank do
 
     it 'parses the base currency with rate 1' do
       expect(subject.currencies[:eur].rate).to eq 1
+    end
+  end
+
+  context 'find currency' do
+    before(:example) do
+      subject.conversion_rates('EUR', {'USD' => 1.11, 'Bitcoin' => 0.0047})
+    end
+
+    let(:currency_by_iso_code){subject.find_currency('Bitcoin')}
+
+    context 'by iso code' do
+      it 'returns the approriate Currency object if the iso code is registered' do
+        expect(currency_by_iso_code).to be_an_instance_of(VlatkoDawanda::Currency)
+        expect(currency_by_iso_code.iso_code).to eq 'Bitcoin'
+        expect(currency_by_iso_code.rate).to eq 0.0047
+      end
+
+      it 'raises an error when currency is not found' do
+        expect{subject.find_currency('MKD')}.to raise_error VlatkoDawanda::UnknownCurrency
+      end
+    end
+
+    context 'by currency object' do
+      let(:currency_by_object){subject.find_currency(currency_by_iso_code)}
+
+      it 'returns the approriate Currency object if the iso code is registered' do
+        expect(currency_by_object).to be_an_instance_of(VlatkoDawanda::Currency)
+        expect(currency_by_object.iso_code).to eq 'Bitcoin'
+        expect(currency_by_object.rate).to eq 0.0047
+      end
+
+      it 'raises an error when currency is not found' do
+        invalid_currency = currency_by_object.instance_variable_set('@rate', currency_by_object.rate + 1)
+        expect{subject.find_currency(invalid_currency)}.to raise_error VlatkoDawanda::UnknownCurrency
+      end
     end
   end
 
